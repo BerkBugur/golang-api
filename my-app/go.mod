b0b245930b73f61db3bdb90ba5c538423cfdabae
Berk Bugur <berk.bugur@gmail.com>
2023-12-16 14:46:50 +0300

    monitoring

--- my-app/go.mod
+++ my-app/go.mod
@@ -58,6 +58,7 @@ require (
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.1 // indirect
 	github.com/prometheus/client_golang v1.17.0
+	github.com/sirupsen/logrus v1.9.3
 	github.com/stretchr/testify v1.8.4
 	github.com/swaggo/files v1.0.1
 	github.com/swaggo/gin-swagger v1.6.0
--- my-app/main.go
+++ my-app/main.go
@@ -9,21 +9,37 @@ import (
 	"github.com/BerkBugur/Go-Project/initializers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sirupsen/logrus"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+var (
+	requestCount = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "http_requests_total",
+			Help: "Total number of HTTP requests",
+		},
+		[]string{"method"},
+	)
+)
+
 // @title Documenting API
 // @version 1
-// @Description CRUD
+// @Description CRUD Operations
 
 func init() {
+	prometheus.MustRegister(requestCount)
 	initializers.LoadEnvVars()
 	initializers.ConnectDB()
 }
 
 func main() {
+	// Logrus
+	logrus.SetFormatter(&logrus.TextFormatter{})
+	//Prometheus
 	http.Handle("/metrics", promhttp.Handler())
 	go func() {
 		log.Fatal(http.ListenAndServe(":8081", nil))
@@ -39,7 +55,12 @@ func main() {
 		tasks.PUT("/:id", controllers.TaskUpdate)
 		tasks.DELETE("/:id", controllers.TaskDelete)
 	}
+	//TESTS
+	logrus.Info("Uygulama başladı")
+	logrus.Warn("Bu bir uyarı mesajıdır")
+	logrus.Error("Bu bir hata mesajıdır")
 
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	r.Run() // listen and serve on 0.0.0.0:8080
+
 }
