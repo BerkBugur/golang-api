3c4e21c0440995ec45b6f065d28f812c342b66dc
Berk Bugur <berk.bugur@gmail.com>
2023-12-16 16:42:25 +0300

    logging

--- my-app/controllers/taskController.go
+++ my-app/controllers/taskController.go
@@ -4,6 +4,7 @@ package controllers
 import (
 	"github.com/BerkBugur/Go-Project/initializers"
 	"github.com/BerkBugur/Go-Project/models"
+	"github.com/sirupsen/logrus"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,6 +18,7 @@ import (
 // @Param description formData string true "Task description"
 // @Param status formData string true "Task status"
 func TaskCreate(c *gin.Context) {
+	logrus.Info("TaskCreate endpoint")
 	var body struct {
 		Title       string `form:"title" binding:"required"`
 		Description string `form:"description" binding:"required"`
@@ -24,6 +26,7 @@ func TaskCreate(c *gin.Context) {
 	}
 
 	if err := c.ShouldBind(&body); err != nil {
+		logrus.WithError(err).Error("Failed to bind request body")
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
@@ -39,10 +42,11 @@ func TaskCreate(c *gin.Context) {
 
 	err := <-resultChan
 	if err != nil {
+		logrus.WithError(err).Error("Failed to process task creation job")
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
-
+	logrus.Infof("Task created. Task ID: %d", task.ID)
 	c.JSON(200, gin.H{"task": task})
 }
 
@@ -53,6 +57,7 @@ func TaskCreate(c *gin.Context) {
 // @Success 200 {array} models.Task
 // @Router /tasks [get]
 func GetAllTask(c *gin.Context) {
+	logrus.Info("GetAllTask endpoint")
 	var tasks []models.Task
 	resultChan := make(chan []models.Task)
 	pool.wg.Add(1)
@@ -63,6 +68,7 @@ func GetAllTask(c *gin.Context) {
 	}()
 
 	tasks = <-resultChan
+	logrus.Infof("Tasks are listed successfully.")
 	c.JSON(200, gin.H{"tasks": tasks})
 }
 
@@ -74,17 +80,19 @@ func GetAllTask(c *gin.Context) {
 // @Success 200 {object} models.Task
 // @Router /tasks/{id} [get]
 func TaskShowByID(c *gin.Context) {
-	// Get ID
-	id := c.Param("id")
+	logrus.Info("TaskShowByID endpoint")
 
+	id := c.Param("id")
 	// Get Task by ID
 	var task models.Task
 	if err := initializers.DB.First(&task, id).Error; err != nil {
+		logrus.WithError(err).Error("Failed to process task show by id (task not found)")
 		c.JSON(404, gin.H{"error": "Task not found"})
 		return
 	}
 
 	// Response
+	logrus.Infof("Task listed successfully.")
 	c.JSON(200, gin.H{"task": task})
 }
 
@@ -100,12 +108,14 @@ func TaskShowByID(c *gin.Context) {
 // @Success 200 {object} models.Task
 // @Router /tasks/{id} [put]
 func TaskUpdate(c *gin.Context) {
+	logrus.Info("TaskUpdate endpoint")
 	// Get ID
 	id := c.Param("id")
 
 	// Get existing task by ID
 	var existingTask models.Task
 	if err := initializers.DB.First(&existingTask, id).Error; err != nil {
+		logrus.WithField("id", id).WithError(err).Error("Failed to get existing task for updating")
 		c.JSON(404, gin.H{"error": "Task not found"})
 		return
 	}
@@ -117,6 +127,7 @@ func TaskUpdate(c *gin.Context) {
 		Status      string `form:"status" binding:"required"`
 	}
 	if err := c.ShouldBind(&updatedData); err != nil {
+		logrus.WithError(err).Error("Failed to bind request body")
 		c.JSON(400, gin.H{"error": err.Error()})
 		return
 	}
@@ -128,11 +139,13 @@ func TaskUpdate(c *gin.Context) {
 
 	// Save the updated task to the database
 	if err := initializers.DB.Save(&existingTask).Error; err != nil {
+		logrus.WithError(err).Error("Failed to update task")
 		c.JSON(500, gin.H{"error": "Failed to update task"})
 		return
 	}
 
 	// Response with the updated task
+	logrus.Infof("Task updated successfully.")
 	c.JSON(200, gin.H{"task": existingTask})
 }
 
@@ -144,15 +157,17 @@ func TaskUpdate(c *gin.Context) {
 // @Success 200
 // @Router /tasks/{id} [delete]
 func TaskDelete(c *gin.Context) {
+	logrus.Info("TaskDelete endpoint")
 	// Get ID
 	id := c.Param("id")
 
 	// Delete Task by ID
 	if err := initializers.DB.Delete(&models.Task{}, id).Error; err != nil {
+		logrus.WithError(err).Error("Failed to delete task")
 		c.JSON(500, gin.H{"error": "Failed to delete task"})
 		return
 	}
-
 	// Response
+	logrus.Infof("Task deleted successfully.")
 	c.Status(200)
 }
